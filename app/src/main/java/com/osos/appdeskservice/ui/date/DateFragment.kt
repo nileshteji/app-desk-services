850619fdc1a3c9f9a01f7ae0e3c58612adca8597
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-11-19 13:08:41 +0530

    Date Module

--- app/src/main/java/com/osos/appdeskservice/ui/date/DateFragment.kt
+++ app/src/main/java/com/osos/appdeskservice/ui/date/DateFragment.kt
@@ -2,18 +2,20 @@ package com.osos.appdeskservice.ui.date
 
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CalendarView
-import android.widget.DatePicker
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.osos.appdeskservice.R
+import com.osos.appdeskservice.data.Places
+import com.osos.appdeskservice.preferences.PreferenceClass
 import kotlinx.android.synthetic.main.fragment_date.*
-import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -21,11 +23,13 @@ import java.util.*
 
 class DateFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = DateFragment()
-    }
+
 
     private lateinit var viewModel: DateViewModel
+    lateinit var city : Places
+    var dt : Long? = null
+    lateinit var unit :String
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,21 +43,73 @@ class DateFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(DateViewModel::class.java)
 
-        calendar_button.setOnClickListener(){
+        init()
 
-            val datePickerDialog=DatePickerDialog(
+
+        // click listeners
+        calendar_button.setOnClickListener() {
+
+            val datePickerDialog = DatePickerDialog(
                 requireContext(),
                 DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                    val dt = convertToUnix(dayOfMonth,month, year)
-
+                    dt = convertToUnix(dayOfMonth, month, year)
                 },
+
                 Calendar.getInstance().get(Calendar.YEAR),
                 Calendar.getInstance().get(Calendar.MONTH),
                 Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
 
             ).show()
 
+        }
 
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long
+            ) {
+                when (position) {
+                    1 -> { city = Places.Delhi }
+                    2 -> { city = Places.Mumbai}
+                    3 -> { city = Places.Noida}
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+
+        submit.setOnClickListener {
+            // checking the date selected
+            if(dt == null){
+                Toast.makeText(requireContext(),"Select Some date",Toast.LENGTH_LONG).show()
+            }
+            else{
+
+                // checking the temp scale
+                if(PreferenceClass(requireContext()).getTemperature().equals("CEL")) {
+                  viewModel.fetchResponse(city,dt!!)
+                }
+                else{
+                    viewModel.fetchResponse(city,dt!!,"imperial")
+                }
+            }
+        }
+
+
+    }
+
+
+    fun init() {
+        ArrayAdapter.createFromResource(
+            requireActivity(),
+            R.array.cities,
+            R.layout.support_simple_spinner_dropdown_item
+        ).also {
+
+            it.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+            spinner.adapter = it
         }
 
 
@@ -62,16 +118,21 @@ class DateFragment : Fragment() {
     }
 
 
-    fun convertToUnix(dayOfMonth:Int , month:Int, year:Int ) :Long{
+    fun convertToUnix(dayOfMonth: Int, month: Int, year: Int): Long {
         val monthNew = month + 1
 
-        val l = LocalDate.parse("$dayOfMonth-$monthNew-$year", DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+        val l = LocalDate.parse(
+            "$dayOfMonth-$monthNew-$year",
+            DateTimeFormatter.ofPattern("dd-MM-yyyy")
+        )
+
+        date.text = "$dayOfMonth $monthNew $year"
 
         val unix = l.atStartOfDay(ZoneId.systemDefault()).toInstant().epochSecond
 
         Log.d("LOGS", "onActivityCreated: $unix")
 
-        return  unix
+        return unix
 
     }
 
--- app/src/main/java/com/osos/appdeskservice/ui/date/DateViewModel.kt
+++ app/src/main/java/com/osos/appdeskservice/ui/date/DateViewModel.kt
@@ -2,22 +2,69 @@ package com.osos.appdeskservice.ui.date
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.osos.appdeskservice.api.ApiBuilder
+import com.osos.appdeskservice.data.Constants
 import com.osos.appdeskservice.data.DateResponse
+import com.osos.appdeskservice.data.Places
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class DateViewModel : ViewModel() {
 
 
+    var history: MutableLiveData<DateResponse> = MutableLiveData()
+    lateinit var lat: String
+    lateinit var long: String
 
 
-    var history :MutableLiveData<DateResponse> = MutableLiveData()
+    fun fetchResponse(place: Places, unix: Long, unit: String = "metric") {
 
 
+        when (place) {
+
+            Places.Delhi -> {
+
+                lat = Constants.delhi_lat
+
+                long = Constants.delhi_long
+
+            }
 
 
-    fun fetchResponse(){
+            Places.Mumbai -> {
+
+                lat = Constants.mumbai_lat
+
+                long = Constants.mumbai_long
+
+            }
+
+
+            Places.Noida -> {
+
+                lat = Constants.noida_lat
+
+                long = Constants.noida_long
+
+            }
+
+
+        }
+
+
+        viewModelScope.launch {
+
+            val job = async {
+                ApiBuilder.getApiCall()
+                    .getHistory(lat, long, unix.toString(), Constants.API_KEY, unit)
+            }
+
+            history.value = job.await()
+
+        }
 
     }
-
 
 
 }
