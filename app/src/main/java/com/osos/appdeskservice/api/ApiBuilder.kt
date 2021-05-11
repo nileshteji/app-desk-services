7317c7a1581b90a93ce9dd5c3b7030607f464da3
Nilesh <kumar.nilesh023@gmail.com>
2021-05-11 23:50:58 +0530

    Final Update

--- app/src/main/java/com/osos/appdeskservice/api/ApiBuilder.kt
+++ app/src/main/java/com/osos/appdeskservice/api/ApiBuilder.kt
@@ -1,6 +1,7 @@
 package com.osos.appdeskservice.api
 
 import com.osos.appdeskservice.data.Constants
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,6 +11,7 @@ object ApiBuilder {
     fun getRetrofit():Retrofit{
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
+            .client(getHTTP())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -17,6 +19,10 @@ object ApiBuilder {
 
     fun getApiCall():ApiCall{
         return getRetrofit().create(ApiCall::class.java)
+    }
+
+    fun getHTTP(): OkHttpClient{
+        return OkHttpClient.Builder().addInterceptor(NetworkInterceptor()).build()
     }
 
 }
